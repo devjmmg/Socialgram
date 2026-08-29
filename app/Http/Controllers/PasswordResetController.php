2be7501eb2cf327e6ce06b59a4890ae7db5be606
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-08-28 21:00:14 -0600

    feat: improve password management

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -11,23 +11,26 @@ class PasswordResetController extends Controller
     
     public function update(Request $request)
     {
-
-        $this->validate($request,[
+        $this->validate($request, [
             'password_current' => 'required',
             'password' => 'required|confirmed|min:8',
+
+        ], [
+            'password_current.required' => 'La contraseña actual es requerida.',
+            'password.required' => 'La nueva contraseña es requerida.',
+            'password.confirmed' => 'Las contraseñas no coinciden.',
+            'password.min' => 'La nueva contraseña debe tener al menos 8 caracteres.',
         ]);
 
-        if(!Hash::check($request->password_current,auth()->user()->password))
-        {
-            return back()->with('password','La contraseña actual es incorrecta');
+        if (!Hash::check($request->password_current, auth()->user()->password)) {
+            return back()->with('password', 'La contraseña actual es incorrecta.');
         }
 
         $user = User::find(auth()->user()->id);
         $user->password = Hash::make($request->password);
         $user->save();
 
-        return back()->with('success','La contraseña se cambio correctamente');
-
+        return back()->with('password_success', 'La contraseña se actualizó correctamente.');
     }
 
 }
